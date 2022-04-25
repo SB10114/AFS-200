72c31e174d1aef712b4a47b89160b6a0b9360ebb
Sierra <sierra.butler@bryanuniversity.edu>
2022-04-24 20:51:01 -0500

    error commit

---
+++ week1/apples.py
@@ -0,0 +1,7 @@
+applePrice = .50
+userName = input("What is your name? ")
+
+applesPurchased = int()
+totalAmount = int()
+
+print('Welcome ' + userName + 'each apple costs ' + applePrice + applesPurchased + 'Thank you ' + userName + 'for purchasing ' + applesPurchased + 'for the amount of ')
