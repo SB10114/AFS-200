b45beef6d90364a54aed84083ac59ef047fd415c
Sierra <sierra.butler@bryanuniversity.edu>
2022-04-26 11:21:41 -0500

    commit 1

--- week2/magic.py
+++ week2/magic.py
@@ -1,4 +1,8 @@
-number = input('Please enter a number')
+number = int(input('Hello, please enter a number'))
 
+mult = number*3
+add = mult+6
+div = add/3
+last = int(div-number)
 
-print(number)
+print(last)
