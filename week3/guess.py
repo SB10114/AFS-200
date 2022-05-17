22baa4c3b0fc2502f4bb1ba45402d7643f210255
Sierra <sierra.butler@bryanuniversity.edu>
2022-05-16 20:02:37 -0500

    commit

--- week3/guess.py
+++ week3/guess.py
@@ -1,3 +1,62 @@
-word = input('pumpkin')
+import random
 
-wordBoard = 
+
+name = input("What is your name? ") 
+print("Good Luck ! ", name)
+ 
+words = ['pineapple', 'alphabet', 'salamandar', 'goose', 'guess']
+ 
+
+word = random.choice(words)
+ 
+ 
+print("Guess a letter")
+ 
+guesses = ''
+ 
+
+turns = 12
+ 
+ 
+while turns > 0:
+    failed = 0
+     
+    for char in word:
+      
+        if char in guesses:
+            print(char, end=" ")
+             
+        else:
+            print("_")
+            print(char, end=" ")
+             
+            failed += 1
+             
+ 
+    if failed == 0:
+       
+        print("You Win")
+         
+        
+        print("The word is: ", word)
+        break
+     
+    print()
+    guess = input("guess a character:")
+     
+
+    guesses += guess
+     
+    if guess not in word:
+         
+        turns -= 1
+         
+     
+        print("Wrong")
+         
+    
+        print("You have", + turns, 'more guesses')
+         
+         
+        if turns == 0:
+            print("You Loose")
